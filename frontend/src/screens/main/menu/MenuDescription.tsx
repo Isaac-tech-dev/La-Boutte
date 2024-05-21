0d49f5b3e62f79a457b89f70fe35e1bff24bc1a1
isaac-tech-01 <isaac.ayeni@optimusbank.com>
2024-05-21 12:33:14 +0100

    Implemented Search on the list of Pizza

--- frontend/src/screens/main/menu/MenuDescription.tsx
+++ frontend/src/screens/main/menu/MenuDescription.tsx
@@ -6,6 +6,15 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { BACK, SAVE } from "../../../svg";
 import { SvgXml } from "react-native-svg";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import {
+  Ionicons,
+  MaterialIcons,
+  Feather,
+  MaterialCommunityIcons,
+  Entypo,
+  FontAwesome5,
+  AntDesign,
+} from "@expo/vector-icons";
 
 type MenuDescriptionScreenProps = NativeStackScreenProps<
   RootStackParamList,
@@ -26,18 +35,37 @@ const MenuDescription = ({ navigation, route }: MenuDescriptionScreenProps) => {
           <SvgXml xml={SAVE} />
         </View>
         {/* Description */}
-        <View className={`flex items-center`}>
+        <View className={`flex items-center space-y-4`}>
           <Image
             source={{ uri: image }}
             width={250}
             height={250}
             className={`rounded-full`}
           />
-          <Text>{name}</Text>
+          <Text className={`text-[24px] font-bold`}>{name}</Text>
+          <View className="flex-row items-center">
+            <Feather name="star" size={14} color="#FE6400" />
+            <Feather name="star" size={14} color="#FE6400" />
+            <Feather name="star" size={14} color="#FE6400" />
+            <Feather name="star" size={14} color="#FE6400" />
+            <Text className="ml-2" style={{ color: "#FE6400" }}>
+              4.5
+            </Text>
+          </View>
+          <View className="flex-row items-center">
+            <View className="flex-row items-center">
+              <FontAwesome5 name="dumbbell" size={12} color="#464646" />
+              <Text className="ml-1">5.0g</Text>
+            </View>
+            <View className="flex-row items-center ml-2">
+              <Ionicons name="flash" size={12} color="#464646" />
+              <Text className="ml-1">60 cal</Text>
+            </View>
+          </View>
         </View>
-        <View className={`flex items-start justify-start text-left`}>
-          <Text>Description</Text>
-          <Text>{description}</Text>
+        <View className={`flex items-start justify-start text-left mt-[10px] space-y-2`}>
+          <Text className={`text-[18px] font-bold`}>Description</Text>
+          <Text className={`text-[10px]`}>{description}</Text>
         </View>
       </View>
     </SafeAreaView>
